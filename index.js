7d11fff1efc3801cd1bbfb78a4af688ccb328b5c
Jason Beedle <jason_beedle@hotmail.com>
2019-03-24 17:31:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ function happyHolidayTo(holiday, name) {
   return 'Happy Independence Day, you!'
 }
 
-
-function (days, holiday) {
-  return 'It\'s 20 days until Mother\'s Day!';
+function holidayCountdown(days, holiday) {
+  return 'Its 20 days until $Mothers Day!'
 }
